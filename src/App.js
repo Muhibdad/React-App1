cb890dc55a07af68105dd28ed9eb06503d9a5d47
Muhibdad <Muhibdadkhan@gmail.com>
2023-04-28 23:58:46 +0500

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>GPT-3</h1>
     </div>
   );
 }
